b3b6dd88eee0d9f77d107a824fdf25889ff572ef
Ugur <ercinugur@gmail.com>
2022-03-10 14:12:41 +0300

    implementing google maps

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,5 @@
+import { Modal } from './UI/modal'
+
 class PlaceFinder {
   constructor() {
     const adressForm = document.querySelector('form');
@@ -14,7 +16,10 @@ class PlaceFinder {
       alert('Location feature does not working. Please enter a location manually.');
       return
     }
+    const modal = new Modal('loading-modal-content', 'Loading location - please wait')
+    modal.show();
     navigator.geolocation.getCurrentPosition(successResult => {
+      modal.hide();
       const coordinates = {
         lat: successResult.coords.latitude,
         lng: successResult.coords.longitude
@@ -22,6 +27,7 @@ class PlaceFinder {
       console.log(coordinates)
     },
      error => {
+       modal.hide();
       alert('Could not locate you, please enter your adress manually.')
     })
   }
